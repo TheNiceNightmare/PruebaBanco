954ae2300f4194456b654bc807fb921f7f589517
Charlotte <thenicenightmare@gmail.com>
2024-03-14 13:45:32 -0500

    first commit

---
+++ Models/DbpruebaContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoLogin.Models;
+
+public partial class DbpruebaContext : DbContext
+{
+    public DbpruebaContext()
+    {
+    }
+
+    public DbpruebaContext(DbContextOptions<DbpruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF9703B0300A");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
